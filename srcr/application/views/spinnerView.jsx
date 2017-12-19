34893575b160014ac3d7a50e18ee775f24284ec1
osboxes.org <dimadulevich@gmail.com>
2017-12-19 11:13:54 +0300

    Added spinner view

--- srcr/application/views/spinnerView.jsx
+++ srcr/application/views/spinnerView.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import './spinnerVIew.css';
 
 export default function spinner() {
-    return <div/>;
+    return <div class="fulfilling-bouncing-circle-spinner">
+        <div class="circle"></div>
+        <div class="orbit"></div>
+  </div>
 }
