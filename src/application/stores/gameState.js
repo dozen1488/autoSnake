5d14f01057194bc971d159a9c2e41c0cf32ae519
dmitry <dimadulevich@gmail.com>
2018-01-08 13:22:15 +0300

    Added state reducers, changed onHover action

---
+++ src/application/stores/gameState.js
@@ -0,0 +1,22 @@
+import { fromJS } from "immutable";
+
+import ActionTypes from "../actions/actionTypes";
+
+export default function reduceGameState(state = fromJS(defaultState), action) {
+    switch (action.type) {
+        case ActionTypes.gameOver:
+            return state.set("isGameOver", true);
+        case ActionTypes.spacePressed:
+            return state.set("isPaused", true);
+        case ActionTypes.networkReady:
+            return state.set("networkReady", true);
+        default:
+            return state;
+    }
+}
+
+const defaultState = {
+    isGameOver: false,
+    isPaused: false,
+    networkReady: false
+};
